9ec8a260502e5247e6fbd8bbe10b2ae80df3f3a8
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-02-26 01:02:18 +0000

    redirect btn and js file edit

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
 });
 
+function redirect(targetId) {
+        var targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        } else {
+            console.error("Elementul cu id-ul " + targetId + " nu a fost găsit.");
+        }
+    }
